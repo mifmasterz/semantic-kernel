7655e925532da18ef21faafdf44b9e71622354b5
mifmasterz <mifmasterz@yahoo.com>
2023-06-23 06:56:52 +0700

    fix issues: Pascal case is typically used for property names in C#, so please capitalize first later of each property of data contracts in this and other files, Unused code, This e is not AIException additional check is not necessary since it's covered by the e.IsCriticalException(),Remove the dead code, There's no need to implement IDIsposabale since all http client created within this class use NonDisposableHttpClientHandler, There's no need to add constructor that is obsolete. Same for the other obsolete constructors/methods of this and the other classes, Add XML comments to the data contracts and their members, Remove empty line, It should be possible to supply an instance of HttpClient if client code/hosting app wants/needs to do so, No need initializing either _model, use camel case for parameter names, consider adding this import line.

--- dotnet/src/Connectors/Connectors.AI.PaLM/Skills/TokenRequest.cs
+++ dotnet/src/Connectors/Connectors.AI.PaLM/Skills/TokenRequest.cs
@@ -1,23 +1,28 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Text.Json.Serialization;
 
 namespace Connectors.AI.PaLM.Skills;
+/// <summary>
+/// text 
+/// </summary>
 public class MessageToken
 {
     [JsonPropertyName("content")]
     public string Content { get; set; }
 }
-
+/// <summary>
+/// contain collection of text for token counting
+/// </summary>
 public class PromptToken
 {
     [JsonPropertyName("messages")]
     public List<MessageToken> Messages { get; set; } = new();
 }
-
+/// <summary>
+/// class for request token count
+/// </summary>
 public class TokenRequest
 {
     [JsonPropertyName("prompt")]
